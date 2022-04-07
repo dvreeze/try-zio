0a77576d8070fdf9f15c6f0619e1e919d8fc22d2
Chris de Vreeze <chris.de.vreeze@caiway.net>
2022-04-07 23:01:18 +0200

    Worked on JdbcSupport

--- src/main/scala/eu/cdevreeze/tryzio/jdbc/JdbcSupport.scala
+++ src/main/scala/eu/cdevreeze/tryzio/jdbc/JdbcSupport.scala
@@ -18,6 +18,9 @@ package eu.cdevreeze.tryzio.jdbc
 
 import java.sql.Connection
 import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.Types
+import java.util.concurrent.atomic.AtomicBoolean
 
 import scala.util.chaining.*
 
@@ -25,9 +28,10 @@ import javax.sql.DataSource
 import zio.*
 
 /**
- * Naive ZIO-based JDBC support, somewhat reminiscent of what Spring JDBC support achieves, but very much simplified and more "functional".
+ * Naive ZIO-based JDBC support, somewhat inspired by Spring JDBC support, but quite minimal and trying to be more "functional". It is also
+ * quite "spartan" in that layers of transactions, connections, statements etc. are "in your face".
  *
- * Note that JDBC code typically must run in a blocking way, using one single thread for each use of a Connection.
+ * Note that JDBC code typically must run in a blocking way, using one single thread for each (transactional) use of a Connection.
  *
  * @author
  *   Chris de Vreeze
@@ -36,18 +40,18 @@ object JdbcSupport:
 
   type ScopedTask[A] = RIO[Scope, A]
 
-  final class Transaction(val connection: Connection, val rollbackOnly: Boolean):
+  final class Transaction(val connection: Connection, val rollbackOnly: AtomicBoolean):
     def isolationLevel: Int = connection.getTransactionIsolation
     def rollback(): Unit = connection.rollback()
     def commit(): Unit = connection.commit()
-    def onlyRollback(): Transaction = Transaction(connection, rollbackOnly = true)
+    def onlyRollback(): Unit = rollbackOnly.set(true)
 
   object Transaction:
-    def unsafe(conn: Connection): Transaction = new Transaction(conn, rollbackOnly = false)
+    def unsafe(conn: Connection): Transaction = new Transaction(conn, rollbackOnly = AtomicBoolean(false))
     def start(conn: Connection, isolationLevel: Int): Transaction =
       new Transaction(
         conn.tap(_.setAutoCommit(false)).tap(_.setTransactionIsolation(isolationLevel)),
-        rollbackOnly = false
+        rollbackOnly = AtomicBoolean(false)
       )
 
   end Transaction
@@ -56,36 +60,29 @@ object JdbcSupport:
     def withIsolationLevel(newIsolationLevel: Int): TransactionConfig =
       this.copy(isolationLevel = newIsolationLevel)
 
-  // TODO More cases
+  enum Argument(val arg: AnyRef, val argType: Int):
+    case TypedArg(override val arg: AnyRef, override val argType: Int) extends Argument(arg, argType)
+    case UntypedArg(override val arg: AnyRef) extends Argument(arg, Types.OTHER)
 
-  enum PreparedStatementConfig(val sqlString: String):
-    case FromQuery(override val sqlString: String) extends PreparedStatementConfig(sqlString)
-
-    def prepareStatementUsing(conn: Connection): PreparedStatement =
-      this match
-        case FromQuery(sql) => conn.prepareStatement(sql)
-  end PreparedStatementConfig
-
-  extension (conn: Connection)
-    def prepareStatement(psConfig: PreparedStatementConfig): PreparedStatement = psConfig.prepareStatementUsing(conn)
-
-  // TODO Make exceptions for which to rollback configurable
+  // TODO Make exception types for which to rollback configurable
 
   final class Transactional(val ds: DataSource, val config: TransactionConfig):
     def withIsolationLevel(newIsolationLevel: Int): Transactional =
       Transactional(ds, config.withIsolationLevel(newIsolationLevel))
 
-    def call[A](f: Transaction => Task[A]): Task[A] =
-      startTransactionAndCall(startTransaction())(f)
+    def execute[A](f: Transaction => Task[A]): Task[A] =
+      execute(startTransaction())(f)
 
-    def startTransactionAndCall[A](acquireTx: => Task[Transaction])(f: Transaction => Task[A]): Task[A] =
+    def execute[A](acquireTx: => Task[Transaction])(f: Transaction => Task[A]): Task[A] =
       val scopedTask: ScopedTask[Transaction] =
         ZIO.acquireRelease(acquireTx)(tx => finishTransaction(tx))
-      // Transaction instance changed. Does this work? No, it does not.
-      ZIO.scoped(scopedTask.flatMap(tx => f(tx).tapError(_ => IO.succeed(tx.onlyRollback()))))
 
-    def query[A](psConfig: PreparedStatementConfig)(f: PreparedStatement => Task[A]): Task[A] =
-      call(tx => use(tx.connection).query(psConfig)(f))
+      ZIO.scoped {
+        scopedTask.flatMap { tx =>
+          f(tx).tapError(_ => IO.succeed(tx.onlyRollback()))
+        }
+      }
+    end execute
 
     // TODO Improve the functions below, and also make isolation level an enum
 
@@ -96,32 +93,49 @@ object JdbcSupport:
       }
 
     private def finishTransaction(tx: Transaction): UIO[Unit] =
-      val commitOrRollback: Task[Unit] = Task.attempt { if tx.rollbackOnly then tx.rollback() else tx.commit() }
+      val commitOrRollback: Task[Unit] = Task.attempt { if tx.rollbackOnly.get then tx.rollback() else tx.commit() }
       commitOrRollback.catchAll(_ => Task.succeed(())).ensuring(Task.succeed(tx.connection.close()))
   end Transactional
 
   final class UsingDataSource(val ds: DataSource):
-    def call[A](f: Connection => Task[A]): Task[A] =
-      acquireConnectionAndCall(Task.attempt(ds.getConnection))(f)
+    def execute[A](f: Connection => Task[A]): Task[A] =
+      execute(Task.attempt(ds.getConnection))(f)
 
-    def acquireConnectionAndCall[A](acquireConn: => Task[Connection])(f: Connection => Task[A]): Task[A] =
+    def execute[A](acquireConn: => Task[Connection])(f: Connection => Task[A]): Task[A] =
       val scopedTask: ScopedTask[Connection] =
         ZIO.acquireRelease(acquireConn)(conn => Task.succeed(conn.close()))
       ZIO.scoped(scopedTask.flatMap(f))
-
-    def query[A](psConfig: PreparedStatementConfig)(f: PreparedStatement => Task[A]): Task[A] =
-      call(conn => use(conn).query(psConfig)(f))
   end UsingDataSource
 
   final class UsingConnection(val conn: Connection):
-    def query[A](psConfig: PreparedStatementConfig)(f: PreparedStatement => Task[A]): Task[A] =
-      acquirePreparedStatementAndCall(Task.attempt(conn.prepareStatement(psConfig)))(f)
+    def execute[A](sqlString: String, args: Seq[Argument])(f: PreparedStatement => Task[A]): Task[A] =
+      execute(createPreparedStatement(sqlString, args))(f)
 
-    def acquirePreparedStatementAndCall[A](acquirePs: => Task[PreparedStatement])(f: PreparedStatement => Task[A]): Task[A] =
+    def execute[A](acquirePs: => Task[PreparedStatement])(f: PreparedStatement => Task[A]): Task[A] =
       val scopedTask: ScopedTask[PreparedStatement] =
         ZIO.acquireRelease(acquirePs)(ps => Task.succeed(ps.close()))
       ZIO.scoped(scopedTask.flatMap(f))
+
+    def query[A](sqlString: String, args: Seq[Argument])(rowMapper: (ResultSet, Int) => A): Task[Seq[A]] =
+      execute(sqlString, args)(ps => queryForResults(ps, rowMapper))
+
+    private def createPreparedStatement(sqlString: String, args: Seq[Argument]): Task[PreparedStatement] =
+      Task.attempt {
+        val ps = conn.prepareStatement(sqlString)
+        args.zipWithIndex.foreach { (arg, index) =>
+          ps.setObject(index + 1, arg.arg, arg.argType)
+        }
+        ps
+      }
+
+    private def queryForResults[A](ps: PreparedStatement, rowMapper: (ResultSet, Int) => A): Task[Seq[A]] =
+      Task.attempt {
+        val rs: ResultSet = ps.executeQuery()
+        Iterator.from(1).takeWhile(_ => rs.next).map(idx => rowMapper(rs, idx)).toSeq // 1-based?
+      }
   end UsingConnection
+
+  // Entrypoints of this API
 
   def transactional(ds: DataSource, config: TransactionConfig): Transactional = Transactional(ds, config)
 
